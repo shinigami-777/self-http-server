b51e7b24b85090b62a8291c300706cc3555e6b67
shinigami-777 <chattopadhyaytamaghna@gmail.com>
2025-01-05 20:40:25 +0530

    bind-port-added

---
+++ app/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main(){
+	fmt.Println("Logs will appear here: ")
+
+	l,err := net.Listen("tcp","0.0.0.0:4221");
+	if err != nil{
+		fmt.Println("Failed to bindd port 4221.")
+		os.Exit(1);
+	}
+	_,err = l.Accept;
+	if err !=nil{
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module self-http-server
+
+go 1.23.3
